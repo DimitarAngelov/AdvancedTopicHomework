914135f628ead994b05efea6de3c7ac75b3d4d62
DimitarAngelov <dimiturangelov@gmail.com>
2015-05-19 18:24:59 +0300

    Regular Expressions Homework
    only first 4 tasks

---
+++ Homework-n0way0ut-Regular Expressions/04.RegularExpressions/04.ExtractingSenteces/ExtractingSentences.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+
+class ExtractingSentences
+{
+    static void Main()
+    {
+        string keyword = Console.ReadLine();
+        string input = Console.ReadLine();
+        string check = string.Format(@"(?<=\s|^)(.*?\b{0}\b.*?(?=\!|\.|\?)[?.!])", keyword);
+        Regex sentences = new Regex(check);
+        MatchCollection fullSentences = sentences.Matches(input);
+        StringBuilder sb = new StringBuilder();
+        foreach (Match Match in fullSentences)
+        {
+            sb.AppendLine(Match.Value);
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
---
+++ Homework-n0way0ut-Regular Expressions/04.RegularExpressions/04.RegularExpressions/ExternalEmails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Text;
+
+class MailExtracter
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        Regex emailRegex = new Regex(@"(?<![._\w+-])(?<user>\w+([-+.]\w+)*)@(?<host>\w+([-.]\w+)*\.\w+([-.]\w+)*)", RegexOptions.IgnoreCase);
+        MatchCollection emailMatches = emailRegex.Matches(input);
+        StringBuilder sb = new StringBuilder();
+        foreach (Match emailMatch in emailMatches)
+        {
+            sb.AppendLine(emailMatch.Value);
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
